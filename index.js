c0c2a8f3240ce8942f415e1be618a110a88c608b
Paul Chibueze <paulpadro03@gmail.com>
2022-06-21 15:35:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const name = "Chibueze Paul"
+const country = "Nigeria"
+let age = "22"
+
+let myID = `${'i am ' + name}, ${'from ' + country} ${'and i am '+ age +' years old.'}`
+
+console.log(myID);
